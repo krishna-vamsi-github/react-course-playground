6fe4a42378796a5746e10dea1b261817daf9ed4d
krishna-vamsi-github <anumala.krishnavamsi@gmail.com>
2023-12-25 12:48:00 +0530

    Fetching Posts data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList></TodoList>;
+  return <PostList></PostList>;
 }
 
 export default App;
