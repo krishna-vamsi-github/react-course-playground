d5d9b47ca7e42ea26ebe3774784adf290b698e14
krishna-vamsi-github <anumala.krishnavamsi@gmail.com>
2023-12-25 07:31:55 +0530

    Creating a Custom Query Hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,28 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-  const { data: todos, error, isLoading } = useQuery<Todo[], Error>({
-    // queryKey is used for caching, whenever data is fecthed from the backend, it is stored in caching
-    queryKey: ["todos"],
-    // queryFn is a function that used to fetch the data from the backend, this function should return promise that
-    // resolves to data or the error . we can use any http or fetch function to fetch the data. becoz react query only
-    // concerns about managing and caching data not about how we fetch it
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
-  if(isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
---
+++ src/react-query/hooks/useTodos.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const useTodos = () => {
+  const fetchTodos = () => {
+    return axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+  };
+  // queryFn is a function that used to fetch the data from the backend, this function should return promise that
+  // resolves to data or the error . we can use any http or fetch function to fetch the data. becoz react query only
+  // concerns about managing and caching data not about how we fetch it
+  // queryKey is used for caching, whenever data is fecthed from the backend, it is stored in caching
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+};
+
+export default useTodos;
