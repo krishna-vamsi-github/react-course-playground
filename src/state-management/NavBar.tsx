82599cf5bd6a252d3baf4528b01a0df97334d240
krishna-vamsi-github <anumala.krishnavamsi@gmail.com>
2024-01-09 00:00:08 +0530

    ADDED

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{tasks.length}</span>
-      
+      <LoginStatus />
     </nav>
   );
 };
